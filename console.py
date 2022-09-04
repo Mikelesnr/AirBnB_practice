a6398ab49b73d5ba3d754cedf0bc6aa31eafeca4
AturiheiBlendon <2020blendon@gmail.com>
2022-09-04 12:17:45 +0300

    pep8-fix

--- console.py
+++ console.py
@@ -1,7 +1,8 @@
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    # prompt = "(hbnb)"
+    prompt = "(hbnb)"
 
     def do_quit(self, args):
         """
@@ -15,38 +16,6 @@ class HBNBCommand(cmd.Cmd):
         """
         return True
 
-    def do_create(self, args):
-        """
-        Creates a new instance of BaseModel
-        """
-        pass
-
-    def do_show(self, args):
-        """
-        Prints the string representation of an instance
-        based on the class name and id
-        """
-        pass
-
-    def do_destroy(self, args):
-        """
-        Deletes an instance based on the class name and id
-        Updates the json file
-        """
-        pass
-
-    def do_all(self, ags):
-        """
-        Prints all string representation of all instances based or not on the class name
-        """
-        pass
-
-    def do_update(self, args):
-        """
-        Updates an instance based on the class name and id by adding or updating attribute
-        Updates the json file
-        """
-        pass
 
 console = HBNBCommand()
 
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+Module: __init__.py
+"""
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
